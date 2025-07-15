554b2ab146b563f95bf8897527b01fecc9e1731c
Ramazan <mramazantrn28@gmail.com>
2025-07-16 02:25:02 +0300

    Create copy_files.py

---
+++ copy_files.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+os.makedirs("Data/preprocessed/AnimeRatings_min_rating7-min_uc10-min_sc10-splitleave_one_out/", exist_ok=True)
+source_path = 'AnimeRecBERT/Data/AnimeRatings/dataset.pkl'
+
+destination_path = 'AnimeRecBERT/Data/preprocessed/AnimeRatings_min_rating7-min_uc10-min_sc10-splitleave_one_out/dataset.pkl'
+
+shutil.move(source_path, destination_path)
+
+source_path2 = 'AnimeRecBERT/Data/AnimeRatings/random-sample_size100-seed98765.pkl'
+
+destination_path2 = 'AnimeRecBERT/Data/preprocessed/AnimeRatings_min_rating7-min_uc10-min_sc10-splitleave_one_out/random-sample_size100-seed98765.pkl'
+
+shutil.move(source_path2, destination_path2)
